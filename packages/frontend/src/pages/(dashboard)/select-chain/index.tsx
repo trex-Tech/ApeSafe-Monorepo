225486c1cdd91ee4d8c24fcdbe5e4c2f25b436d4
Kelly Owoju <kelliebrighty@gmail.com>
2024-10-26 09:22:43 +0100

    fix: multi chain selector text

--- packages/frontend/src/pages/(dashboard)/select-chain/index.tsx
+++ packages/frontend/src/pages/(dashboard)/select-chain/index.tsx
@@ -4,10 +4,10 @@ const index = () => {
 	return (
 		<div className={`mt-[58px] px-[5%]`}>
 			<div>
-				<p className={`text-center text-[24px] text-[#fff] lg:text-left`}>Select Chain to Deploy</p>
+				<p className={`text-center text-[24px] text-[#fff] lg:text-left`}>Token Live on Base Testnet!</p>
 				<p className={`text-center text-[13px] text-[#fff] lg:w-[668px] lg:text-left`}>
-					You've successfully deployed on Base Mainnet! Now, select at least two additional chains to extend
-					your token's reach.
+					Unlike traditional Signle Chain Tokens, yours can exist everywhere in one simple step. <br />
+					Select up to 5 chains to extend your token's reach.
 				</p>
 
 				<MultiChainSelector />
